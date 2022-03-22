85d5a57791f13b4c712322719216df85882cbf12
johmert <johmert@users.noreply.github.com>
2022-03-21 22:40:12 -0700

    Sum Pairs completed

--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,19 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+    const numbers = new Map();
+    const solution = new Map();
+    A.forEach(e => {
+        numbers.set(e);
+        const diff = N - e;
+        if(diff !== e){
+            if(numbers.has(diff)){
+                solution.set(e, diff);
+            }
+        }
+    });
+    return Array.from(solution);
+}
 
 module.exports = sumPairs;
