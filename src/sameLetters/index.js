b9a9a771e47af58a655e513c612c6303db8d0f82
johmert <johmert@users.noreply.github.com>
2022-03-21 23:08:34 -0700

    same letters completed

--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -2,6 +2,20 @@
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+    const results = new Map();
+    words.forEach(word => {
+        word.toLowerCase();
+        const letters = Array.from(new Set(word.split("")))
+            .sort()
+            .join("");
+        if(results.has(letters)){
+            results.get(letters).push(word);
+        } else {
+            results.set(letters, [word]);
+        }
+    });
+    return results; 
+}
 
 module.exports = sameLetters;
